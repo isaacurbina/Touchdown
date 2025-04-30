860878f8543be53506f3a3dbd31b4b682e800aef
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-30 18:23:07 -0500

    first commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Isaac Urbina on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
